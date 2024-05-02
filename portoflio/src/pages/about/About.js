bd4aa2ac029bbb7d365aaaaa1878c75d1e1177db
Kyle Geary <142063304+GearBots@users.noreply.github.com>
2024-05-01 23:01:55 -0600

    updated about page

--- portoflio/src/pages/about/About.js
+++ portoflio/src/pages/about/About.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1> About Me</h1>
-            <p>I'm Kyle Geary, a full stack devekloper based in Denver, Colorado. </p>
-            <p>With a passion for building and designing clean user-friendly applications, I specialize in bringing  full stack applications to life. </p>
-            <p>I'm always looking for new and exciting projects to work on.</p>
+        <div className='bg-zinc-900  self-center flex flex-col justify-center items-center min-h-screen flex-grow'>
+            <h1 className='text-white text-5xl'> About Me</h1>
+            <p className='text-white text-1xl m-2'>I'm Kyle Geary, a full stack devekloper based in Denver, Colorado. </p>
+            <p className='text-white text-1xl m-2'>With a passion for building and designing clean user-friendly applications, I specialize in bringing  full stack applications to life. </p>
+            <p className='text-white text-1xl m-2'>I'm always looking for new and exciting projects to work on.</p>
             
         </div>
     );
