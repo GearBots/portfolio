b52d385001ca481cc042626949be9b4ecfd9a346
Kyle Geary <142063304+GearBots@users.noreply.github.com>
2024-05-01 18:40:14 -0600

    updated styling on home page

--- portoflio/src/pages/home/Home.js
+++ portoflio/src/pages/home/Home.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <section id="home" className=" bg-zinc-900 flex flex-col justify-center items-center min-h-64  ">
+        <section id="home" className=" bg-zinc-900 flex flex-col justify-center items-center min-h-screen flex-grow ">
             <div class="text-center">
-                <h2 className='text-white text-5xl m-3 py-3'>Hi, I'm Kyle Geary</h2>
-                <h4 className='text-white text-2xl m-3 py-3'> Full-Stack Developer</h4>
+                <h2 className='text-white text-5xl m-2 py-1'>Hi, I'm Kyle Geary</h2>
+                <h4 className='text-white text-2xl m-2'> Full-Stack Developer</h4>
                 <button className='bg-zinc-800 hover:bg-stone-950 text-white font-bold py-2 px-4 rounded m-10'>Contact Me</button>
                 
             </div>
