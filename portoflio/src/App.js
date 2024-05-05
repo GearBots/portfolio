77becad8e2bad8b323eab7a91a5c7cd4dba6256b
Kyle Geary <142063304+GearBots@users.noreply.github.com>
2024-05-05 15:49:58 -0600

    added skills folder and routed skills page to app, basic skills page

--- portoflio/src/App.js
+++ portoflio/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavLayout from './components/NavLayout';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
+import Skills from './pages/skills/Skills';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route path = "/" element = {<NavLayout />}>
         <Route index element = {<Home />} />
         <Route path = "/about" element = {<About />}/>
+        <Route path = "/skills" element = {<Skills />}/>
 
       </Route>
     )
---
+++ portoflio/src/pages/skills/Skills.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div className='bg-zinc-900 self-center flex flex-col justify-center items-center min-h-screen flex-grow'>
+            <h1 className='text-white underline text-4xl mb-4'>Front-End Skills</h1>
+            <p className='text-white text-1xl m-2'>This will be a list of front-end skills</p>
+            <h1 className='text-white underline text-4xl mb-4'>Back-End Skills</h1>
+            <p className='text-white text-1xl m-2'>This will be a list of back-end skills</p>
+        </div>
+    );
+};
+
+export default Skills;
