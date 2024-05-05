cee480593f05093b00d3aa7f4479ee84162f1177
Kyle Geary <142063304+GearBots@users.noreply.github.com>
2024-05-05 15:43:19 -0600

    added some styling

--- portoflio/src/pages/about/About.js
+++ portoflio/src/pages/about/About.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='bg-zinc-900  self-center flex flex-col justify-center items-center min-h-screen flex-grow'>
-            <h1 className='text-white text-5xl'> About Me</h1>
-            <p className='text-white text-1xl m-2'>I'm Kyle Geary, a full stack devekloper based in Denver, Colorado. </p>
-            <p className='text-white text-1xl m-2'>With a passion for building and designing clean user-friendly applications, I specialize in bringing  full stack applications to life. </p>
-            <p className='text-white text-1xl m-2'>I'm always looking for new and exciting projects to work on.</p>
-            
-        </div>
+        // <div className='bg-zinc-900 self-center flex flex-col justify-center items-center min-h-screen flex-grow'>
+            // <h1 className='text-white underline text-4xl mb-4'>About Me</h1>
+            <div className='bg-zinc-900 self-center flex flex-col justify-center items-center min-h-screen flex-grow'>
+                <h1 className='text-white underline text-4xl mb-4'>About Me</h1>
+                <p className='text-white text-1xl m-2'>I'm Kyle Geary, a full stack developer based in Denver, Colorado. </p>
+                <p className='text-white text-1xl m-2'>With a passion for building and designing clean user-friendly applications, I specialize in bringing full stack applications to life. </p>
+                <p className='text-white text-1xl m-2'>I'm always looking for new and exciting projects to work on.</p>
+                
+            </div>
+        // </div>
     );
 };
 
